116fc025847a556f2045218dbea23e52f82d47fa
Дмитрий Зарубинский <dizarubinskiy@gmail.com>
2025-12-30 23:48:25 +0300

    Modal

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, {useState} from 'react'
 import './index.scss';
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  const plus = (e) => {
-    setCounter(counter + 1)
-  }
-
-  const minus = (e) => {
-    setCounter(counter - 1)
-  }
+  
 
   
 
@@ -18,10 +10,7 @@ const App = () => {
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>{counter}</h1>
-        <button className="minus" onClick={minus}>- Минус</button>
-        <button className="plus" onClick={plus}>Плюс +</button>
+        Модальное окно
       </div>
     </div>
   );
